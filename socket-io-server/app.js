ec2a060add04eebbd3d42606093f122d4f7bb4b3
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2020-08-27 01:34:33 -0700

    Implemented the full chat program

--- socket-io-server/app.js
+++ socket-io-server/app.js
@@ -1,35 +1,26 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+const express = require("express")();
+const http = require("http").createServer(express);
+const io = require("socket.io")(http);
 
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+let people = [];
 
-const app = express();
-app.use(index);
-
-const server = http.createServer(app);
-
-const io = socketIo(server);
-
-let interval;
-
-io.on("connection", (socket) => {
-    console.log("New client connected");
-    if (interval){
-        clearInterval(interval);
-    }
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
+io.on('connection', (socket) => {
+    const { id } = socket.client;
+    console.log(`User connected: ${id}`);
+    people.push(id)
+    console.log(people);
+    socket.on("chat message", ({nickname, msg}) => {
+        console.log(`${id}: ${msg}`);
+        io.emit("chat message", {nickname ,msg})
     });
+    socket.on('disconnect', () => {
+        people.splice(people.indexOf(id), 1);
+        console.log(people);
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
-
-const getApiAndEmit = socket => {
-    const response = new Date();
-
-    socket.emit("FromAPI", response);
-};
-
-server.listen(port, () => console.log(`Listening on port ${port}`))
